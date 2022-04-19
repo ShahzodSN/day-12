05ecd73becf6ad3d2fa5621491214d3a9b0e744b
shahzod <shahzodnematov33@gmail.com>
2022-04-19 20:36:52 +0500

    task

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*
+let xabar = "salom bolalar yaxshimisizlar";
+
+function toAllCapitalize(text) {
+  const resultArray = [];
+  let splittedText = text.split(" ");
+  for (word of splittedText) {
+    let firstLetter = word.charAt(0).toUpperCase();
+    resultArray.push(word.replace(word[0], firstLetter));
+  }
+  return resultArray.join(" ");
+}
+
+console.log(toAllCapitalize(xabar));
+*/
+
+/*
+let arr = [5, 2, 1, -10, 8];
+function manfiy(l, r) {
+  return r - l;
+}
+function musbat(l, r) {
+  return l - r;
+}
+console.log(arr.sort(manfiy));
+console.log(arr.sort(musbat));
+*/
+
+let num = [1, 2, 33, 55];
+num.reverse();
+console.log(num);
